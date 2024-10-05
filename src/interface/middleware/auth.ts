cdef52048f130f63e57ae679bc5dd27273d0a144
rafaapcode <rafaap2003@gmail.com>
2024-10-05 13:01:08 -0300

    feat: adding log in the middleware

--- src/interface/middleware/auth.ts
+++ src/interface/middleware/auth.ts
@@ -1,25 +1,39 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import { logger } from "../../infraestructure/logger";
 
-export function authenticateMiddleware(req: Request, res: Response, next: NextFunction) {
- try {
-  const token = req.headers["authorization"]?.split(" ")[1];
-  if(!token) {
-    res.status(401).json({message: "Token is required"});
+export function authenticateMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const token = req.headers["authorization"]?.split(" ")[1];
+    logger.info("Token", token);
+    if (!token) {
+      logger.info("token dont exsits", token);
+      res.status(401).json({ message: "Token is required" });
+      return;
+    }
+
+    const verifiedToken = jwt.verify(token, process.env.JWT_SECRET as string);
+
+    logger.info("Token payload", verifiedToken);
+    if (!verifiedToken) {
+      logger.info("Token verified dont exist", verifiedToken);
+      res.status(401).json({ message: "Token invalido" });
+      return;
+    }
+
+    next();
+  } catch (error: any) {
+    logger.info("Error in the middleware", error.message);
+    res
+      .status(500)
+      .json({
+        message:
+          "Ocorreu um erro no seu login , entre em contato com o suporte.",
+      });
     return;
   }
-
-  const verifiedToken = jwt.verify(token, process.env.JWT_SECRET as string);
-
-  if(!verifiedToken) {
-    res.status(401).json({message: "Token invalido"});
-    return;
-  }
-
-  next();
- } catch (error) {
-    res.status(500).json({message: "Ocorreu um erro no seu login , entre em contato com o suporte."});
-    return;
- }
-
-} 
+}
