48c97329a02ab41efd9392b0ad91f2da37ef60ad
rafaapcode <rafaap2003@gmail.com>
2024-10-05 12:47:15 -0300

    feat: puttin the auth middleware in the userRoutes

--- src/interface/routes/userRoutes.ts
+++ src/interface/routes/userRoutes.ts
@@ -3,6 +3,7 @@ import { Request, Response, Router } from "express";
 import { MongoUserRepository } from "../../infraestructure/repositories/MongoUserRepository";
 import { UserUseCase } from "../../use-cases/UserUseCase";
 import { UserController } from "../controllers/UserController";
+import { authenticateMiddleware } from "../middleware/auth";
 
 const routes = Router();
 
@@ -11,7 +12,7 @@ const mongoRepo = new MongoUserRepository(prisma);
 const userUseCase = new UserUseCase(mongoRepo);
 const userController = new UserController(userUseCase);
 
-routes.patch("/:email", (req: Request, res: Response) =>
+routes.patch("/:email", authenticateMiddleware, (req: Request, res: Response) =>
   userController.updateCpf(req, res)
 );
 
