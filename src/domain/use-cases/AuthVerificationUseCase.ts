bd8f17717e18ac0219a4bf97bb34dd6e673b6097
rafaapcode <rafaap2003@gmail.com>
2024-10-12 07:22:13 -0300

    feat: adding logs in the verificationAuth

--- src/domain/use-cases/AuthVerificationUseCase.ts
+++ src/domain/use-cases/AuthVerificationUseCase.ts
@@ -1,8 +1,18 @@
+import { logger } from "../../infraestructure/logger";
 import { JWTTokenRepository, ResponseAuthVerification } from "../interfaces/JwtTokenRepository";
 
 export class AuthVerificationUseCase {
   constructor(private jwtToken: JWTTokenRepository) {}
   isAuth(token: string): ResponseAuthVerification {
+    if(!token) {
+      logger.warn("Token não recebido");
+      return {
+        error: true,
+        message: "Token must be required",
+        isAuthenticate: false
+      }
+    }
+    logger.info(token);
     return this.jwtToken.verifyToken(token);
   }
 }
--- src/presentation/routes/verificationRoutes.ts
+++ src/presentation/routes/verificationRoutes.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
 import { ExpressAuthenticateAdapter } from "../../adapters/verifyAuthenticationAdapter/expressAuthenticateAdapter";
 import { AuthVerificationUseCase } from "../../domain/use-cases/AuthVerificationUseCase";
-import { JwtUseCase } from "../../domain/use-cases/JwtUseCase";
 import { JwtToken } from "../../infraestructure/jwtToken";
 import { VerifyAuthenticationController } from "../controllers/VerifyAuthenticationController";
 
 const router = Router();
 
 const jwtToken = new JwtToken();
-const jwtUseCase = new JwtUseCase(jwtToken);
-const verificationUseCase = new AuthVerificationUseCase(jwtUseCase);
+const verificationUseCase = new AuthVerificationUseCase(jwtToken);
 const verifyAuthController = new VerifyAuthenticationController(verificationUseCase);
 
 router.get("/", ExpressAuthenticateAdapter.isAuth(verifyAuthController))
