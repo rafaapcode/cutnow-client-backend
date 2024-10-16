932af071c69e40ed393334f0b644f242d29d1271
rafaapcode <rafaap2003@gmail.com>
2024-10-16 13:42:09 -0300

    feat: adding log in the error

--- src/presentation/middleware/auth.ts
+++ src/presentation/middleware/auth.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { logger } from "../../infraestructure/logger";
 
 export function authenticateMiddleware(
   req: Request,
@@ -9,25 +8,21 @@ export function authenticateMiddleware(
 ) {
   try {
     const token = req.headers["authorization"]?.split(" ")[1];
-    logger.info("Token", token);
     if (!token) {
-      logger.info("token dont exsits", token);
       res.status(401).json({ message: "Token is required" });
       return;
     }
 
     const verifiedToken = jwt.verify(token, process.env.JWT_SECRET as string);
 
-    logger.info("Token payload", verifiedToken);
     if (!verifiedToken) {
-      logger.info("Token verified dont exist", verifiedToken);
       res.status(401).json({ message: "Token invalido" });
       return;
     }
 
     next();
   } catch (error: any) {
-    logger.info("Error in the middleware", error.message);
+    console.log("ERROR MIDDLEWARE AUTHENTICATE ", error.message);
     res
       .status(500)
       .json({
